4e04b4febbdf16300f93c1c9448d99f51e6cbfd0
niteesh <74418711+Niteesh-Gundarapu@users.noreply.github.com>
2026-05-16 07:26:24 +0530

    Set basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   transpilePackages: ["three"],
+    basePath: 'Niteesh.github.io',
+  assetPrefix: 'Niteesh.github.io',
 };
 
 export default nextConfig;
